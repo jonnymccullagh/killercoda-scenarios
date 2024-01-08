8c1a6cba7cf2d303d62c826c409aa6c0a0415394
Jonny McCullagh <jonny.mccullagh@redbranch.net>
2024-01-08 15:50:07 +0000

    use kube version as a variable

--- kubernetes-admin/setup.sh
+++ kubernetes-admin/setup.sh
@@ -8,7 +8,7 @@ sleep 1
 
 # Break the Controller Manager
 KUBE_VERSION=$(kubectl version | grep Server | awk '{print $3}')
-sed -i 's#registry.k8s.io/kube-controller-manager:$KUBE_VERSION#registry.k8s.io/kube-controller-maaaaaanager:$KUBE_VERSION#' /etc/kubernetes/manifests/kube-controller-manager.yaml
+sed -i "s#registry.k8s.io/kube-controller-manager:$KUBE_VERSION#registry.k8s.io/kube-controller-maaaaaanager:$KUBE_VERSION#" /etc/kubernetes/manifests/kube-controller-manager.yaml
 sleep 3
 kubectl create namespace marketing
 kubectl create deployment -n marketing campaign --image=nginx:alpine --replicas=3
